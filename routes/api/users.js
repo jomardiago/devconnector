bf66ef5a0ae926cddd1b8ccdc317d4f2e28e52c5
jadiago <jadiago@cpi.com.ph>
2020-01-20 13:52:30 +0800

    Add jsonwebtoken.

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -29,7 +31,14 @@ router.post('/', [
         user.password = await bcrypt.hash(password, salt);
 
         await user.save();
-        res.send('User Registered');
+        
+        const payload = { user: { id: user.id } };
+
+        jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+            if (err) throw err;
+
+            res.json({ token });
+        });
     } catch (error) {
         console.log(error.message);
         res.status(500).send('Server error');
