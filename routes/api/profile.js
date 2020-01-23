cb735e3c83275475ab2b27cb6c1dc7e207c893b5
jadiago <jadiago@cpi.com.ph>
2020-01-23 10:38:50 +0800

    Add put route to add new experience.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -103,9 +103,34 @@ router.delete('/', auth, async (req, res) => {
     try {
         await Profile.findOneAndRemove({ user: req.user.id });
         await User.findOneAndRemove({ _id: req.user.id });
-        res.json({ msg: 'User removed' });
+        res.json({ msg: 'User deleted' });
     } catch (err) {
         console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @acess   Private
+router.put('/experience', [auth, [
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
+    const { title, company, location, from, to, current, description } = req.body;
+    const newExperience = { title, company, location, from, to, current, description };
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        profile.experience.unshift(newExperience);
+        await profile.save();
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
         res.status(500).send('Server Error');
     }
 });
